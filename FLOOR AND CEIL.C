bcd3cdfec2d5a6d37da38f2929bb11ab5b6e38e7
Anuj Kumar <98866899+anujjsengar@users.noreply.github.com>
2023-02-21 22:32:02 +0530

    Create FLOOR AND CEIL.C

---
+++ FLOOR AND CEIL.C
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+//CONVERSION TO FLOOR AND CIEL OF INPUTED NUMBER
+int main() {
+    double a;
+    scanf("%lf",&a);
+    printf("%lf\n",floor(a));
+    printf("%lf",ceil(a));
+    return 0;
+}
