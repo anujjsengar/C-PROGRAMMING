bfc6323b5c079977d6a9b46c0f84211d8257d631
Anuj Kumar <98866899+anujjsengar@users.noreply.github.com>
2023-02-21 22:33:18 +0530

    Create FEET TO INCHES.C

---
+++ FEET TO INCHES.C
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+// CONVERSION OF FEET INTO INCHES AD PER USER INPUT
+int main() {
+    int a;
+    scanf("%d",&a);
+    printf("%d",a*12);
+    return 0;
+}
