6be070999ef713a063cb03c0b9505b4e39330830
Anuj Kumar <98866899+anujjsengar@users.noreply.github.com>
2023-02-21 22:36:33 +0530

    Create SWAPPING.C

---
+++ SWAPPING.C
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+// SWAPPINF OF TWO NUMBERS
+int main() {
+    int a,b;
+    scanf("%d%d",&a,&b);
+    printf("%d %d",b,a);
+    return 0;
+}
