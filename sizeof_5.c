acba49a1aaf1e93010fa8162f5cc6a3233e00315
Anuj Kumar <98866899+anujjsengar@users.noreply.github.com>
2023-02-22 19:45:37 +0530

    Create sizeof_5.c

---
+++ sizeof_5.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main(){
+printf("ch occupied %ld size of char  %ld\n",sizeof(1),sizeof(char));
+printf("b  occupied %ld size of int   %ld\n",sizeof(2),sizeof(int));
+printf("a  occupied %ld size of float %ld\n",sizeof(5),sizeof(float));
+return 0;  
+}
