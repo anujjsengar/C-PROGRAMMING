8e5c100d27846c55b43b229347ec7318de7bf3ef
Anuj Kumar <98866899+anujjsengar@users.noreply.github.com>
2023-05-15 23:01:36 +0530

    Create 2d_array_static_print.c

---
+++ 2d_array_static_print.c
@@ -0,0 +1,12 @@
+// PROGRAM TO DEFINE STATIC 2D ARRAY  ITS IN FORM OF MATRIX
+
+void main()
+{
+    int a[2][2]={{1,2},{2,4}};
+    for(int i=0;i<2;i++){
+        for(int j=0;j<2;j++){
+            printf("  %d",a[i][j]);
+        }
+        printf("\n");
+    }
+}
