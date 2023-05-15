d3a991727e2a6fb5e381ea34b9e13dec277bc0d0
Anuj Kumar <98866899+anujjsengar@users.noreply.github.com>
2023-05-15 22:56:46 +0530

    Create 2d_array_diagonal_print.c

---
+++ 2d_array_diagonal_print.c
@@ -0,0 +1,33 @@
+//PROGRAM TO TAKE 2D ARRAY INPUT FROM USER AND PRINT OUTPUT IN MATRIX FORM AND PRINT ITS DIAGONAL ELEMENT IN FORM OF DIAGONAL
+
+#include <stdio.h>
+
+int main()
+{
+    int a[4][4];
+    for(int i=0;i<4;i++){
+        for(int j=0;j<4;j++){
+            scanf("%d",&a[i][j]);
+        }
+    }
+    for(int i=0;i<4;i++){
+        for(int j=0;j<4;j++){
+            printf("   %d",a[i][j]);
+        }
+        printf("\n");
+    }
+    printf("\n");
+    printf("\n");
+    printf("\n");
+    for(int i=0;i<4;i++){
+        for(int j=0;j<4;j++){
+            if(i==j){
+                printf(" %d",a[i][j]);
+            }
+            else{
+                printf("  ");
+            }
+        }
+        printf("\n");
+    }
+    return 0;
