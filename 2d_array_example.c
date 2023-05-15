69c2bee906652624aedd55cacdfa41fe0a679ee2
Anuj Kumar <98866899+anujjsengar@users.noreply.github.com>
2023-05-15 23:03:44 +0530

    Create 2d_array_example.c

---
+++ 2d_array_example.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+void main()
+{
+    int a[10],f[20]={0};
+    for(int i=0;i<10;i++)
+    scanf("%d",&a[i]);
+    int x=a[0];
+    for(int j=0;j<10;j++){
+        if(x<a[j])
+        x=a[j];
+    }
+    x++;
+   
+    for(int k=0;k<10;k++)
+    f[a[k]]++;
+    for(int m=0;m<10;m++){
+        if(f[m]==1){
+        printf("%d-%d\n",m,f[m]);
+    }
+    }
+}
